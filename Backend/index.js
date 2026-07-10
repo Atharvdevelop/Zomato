f894d2216fac8339fd9f4323eff776a026989433
Atharv Chaturvedi <atharv88192@gmail.com>
2026-07-10 16:03:25 +0530

    feat: initialize Express server with database connection and route configurations

--- Backend/index.js
+++ Backend/index.js
@@ -4,18 +4,22 @@ const db = require('./Config/db');
 const userRoutes = require('./Routes/UserRoutes');
 const productRoutes = require('./Routes/ProductRoutes');
 const app = express();
-const PORT = 32404;
+const PORT = 8006;
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.get("/", (req, res) => {
+    res.send("Zomato Backend is running successfully 🚀");
+});
 
 app.use('/images', express.static('public/images'));
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 db.authenticate().then(() => {
     console.log('Database connected successfully');
-    db.sync({ force: true });
+    db.sync({ alter: true });
     // Set force to true if you want to drop existing tables and create new ones
 }).catch((error) => {
     console.error('Database connection failed:', error);
