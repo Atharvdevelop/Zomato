90bfe9ebe40e95f2f053faa05b3b37090326ccb0
Atharv Chaturvedi <atharv88192@gmail.com>
2026-07-10 16:03:41 +0530

    feat: initialize express server with cors, middleware, and database connectivity

--- Backend/index.js
+++ Backend/index.js
@@ -10,16 +10,12 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-app.get("/", (req, res) => {
-    res.send("Zomato Backend is running successfully 🚀");
-});
-
 app.use('/images', express.static('public/images'));
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 db.authenticate().then(() => {
     console.log('Database connected successfully');
-    db.sync({ alter: true });
+    db.sync({ force: true });
     // Set force to true if you want to drop existing tables and create new ones
 }).catch((error) => {
     console.error('Database connection failed:', error);
