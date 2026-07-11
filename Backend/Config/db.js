80f828836fa6fdd13c1cc1c7125cd2c7fe9f25d3
Atharv Chaturvedi <atharv88192@gmail.com>
2026-07-11 10:26:35 +0530

    feat: initialize backend project with Express and MongoDB configuration

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,9 +1,14 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize('railway', 'root', 'eBEAbcAyLZCpyvaPLEsejrbLMJTZnAvo', {
-    host: 'hayabusa.proxy.rlwy.net',
-    port: 32404,
-    dialect: 'mysql',
-});
+const db = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+    }
+);
 
 module.exports = db;
 
