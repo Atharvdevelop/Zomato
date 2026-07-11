b6fd5003b731257a69cf4d01b1296c1668fc9288
Atharv Chaturvedi <atharv88192@gmail.com>
2026-07-11 14:43:25 +0530

    feat: initialize Sequelize database connection with environment variables

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME || process.env.MYSQLDATABASE,
+    process.env.DB_USER || process.env.MYSQLUSER,
+    process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        host: process.env.DB_HOST || process.env.MYSQLHOST,
+        port: process.env.DB_PORT || process.env.MYSQLPORT,
         dialect: 'mysql',
     }
 );
